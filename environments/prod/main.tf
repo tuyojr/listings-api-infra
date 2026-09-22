12c441ad42bc9218f45991cd9bc258c1f099e8c0
tuyojr <olutuyod@gmail.com>
2026-09-22 22:00:39 +0100

    pass explicit availability_zones to network module

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -14,7 +14,7 @@ module "network" {
 
   name_prefix        = local.name_prefix
   vpc_cidr           = "10.20.0.0/16"
-  az_count           = 2
+  availability_zones = ["us-east-1a", "us-east-1b"]
   single_nat_gateway = false
 
   tags = local.tags
