bde0135400d53918b35f87eb930bb6543e41ef28
tuyojr <olutuyod@gmail.com>
2026-09-22 20:24:35 +0100

    required tf version and s3 backend

---
+++ environments/dev/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.16.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.66.0"
+    }
+  }
+
+  backend "s3" {
+    # Values provided via -backend-config flags in the workflow.
+  }
+}
