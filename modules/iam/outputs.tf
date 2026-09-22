91e0f9f5083b0fb916d7aab493ec4ba2f922af7a
tuyojr <olutuyod@gmail.com>
2026-09-22 20:24:35 +0100

    useful outputs

---
+++ modules/iam/outputs.tf
@@ -0,0 +1,9 @@
+output "execution_role_arn" {
+  value       = aws_iam_role.ecs_task_execution.arn
+  description = "ARN of the shared ECS task execution role"
+}
+
+output "task_role_arns" {
+  value       = { for k, v in aws_iam_role.task : k => v.arn }
+  description = "Map of service name to its task role ARN"
+}
